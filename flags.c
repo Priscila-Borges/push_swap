cbc1e58743dc84a5c80135e01592b11db7a9f118
Priscila_Borges <priscila.borges.nl@gmail.com>
2026-05-06 16:56:04 +0200

    fixed bench print

--- flags.c
+++ flags.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   flags.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bandrade <bandrade@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/04 12:58:30 by pride-ol          #+#    #+#             */
-/*   Updated: 2026/05/04 18:42:22 by bandrade         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   flags.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bandrade <bandrade@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/05/04 12:58:30 by pride-ol      #+#    #+#                 */
+/*   Updated: 2026/05/06 14:35:40 by pride-ol      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,12 @@ static void	print_bench_header(t_config *config, int total_ops)
 	ft_putstr_fd("%\n", 2);
 	if (config->strategy == 1)
 		ft_putstr_fd("[bench] strategy: Simple Sort / O(n^2)\n", 2);
+	else if (config->strategy == 2)
+		ft_putstr_fd("[bench] strategy: Chunk Sort / O(n*sqrt(n))\n", 2);
 	else if (config->strategy == 3)
 		ft_putstr_fd("[bench] strategy: Radix Sort / O(n log n)\n", 2);
 	else
-		ft_putstr_fd("[bench] strategy: Adapt / O(n*sqrt(n))\n", 2);
+		ft_putstr_fd("[bench] strategy: Adaptive (Auto Select)\n", 2);
 	ft_putstr_fd("[bench] total_ops: ", 2);
 	ft_putnbr_fd(total_ops, 2);
 	ft_putstr_fd("\n", 2);
